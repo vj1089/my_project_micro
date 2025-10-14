e4165770acd5947d63f35fd8a7ea0fd77fe1ffff
Vaibhav Dinesh Jain (Microland) <Vaibhav.Jain@microland.com>
2025-10-14 16:29:40 +0530

    Update Ouptut to show NLB details as well

--- elb/Output.tf
+++ elb/Output.tf
@@ -5,16 +5,16 @@ output "LB_Details" {
         LB_Name  = aws_lb.this.tags_all["Name"]
         TG_ID    = aws_lb_target_group.this[*].id
         TG_PORT  = aws_lb_target_group.this[*].port
-        LIST_ID  = var.load_balancer_type == "application" ? aws_lb_listener.this[*].id : []
-        LIST_PORT = var.load_balancer_type == "application" ? aws_lb_listener.this[*].port : []
+        LIST_ID  = aws_lb_listener.this[*].id
+        LIST_PORT = aws_lb_listener.this[*].port
     }
 }
 
 
 # Listener ARNs and Ports (for ALB)
 output "listener_arns" {
-    value = var.load_balancer_type == "application" ? aws_lb_listener.this[*].arn : []
+    value = aws_lb_listener.this[*].arn
 }
 output "listener_ports" {
-    value = var.load_balancer_type == "application" ? aws_lb_listener.this[*].port : []
+    value = aws_lb_listener.this[*].port
 }
