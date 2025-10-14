f22dbb1a70f1338b9a912ed3e354b26997ca419a
Vaibhav Dinesh Jain (Microland) <Vaibhav.Jain@microland.com>
2025-10-14 20:00:14 +0530

    Added Tags in Ouput

--- ec2-instance/Output.tf
+++ ec2-instance/Output.tf
@@ -6,9 +6,26 @@ output "EC2_Details" {
         IP = aws_instance.server.private_ip
         Instance_SG_ID = aws_security_group.sg.id
         Instance_SG_Name = aws_security_group.sg.tags_all["Name"]
+        Instance_Tags = aws_instance.server.tags_all
+        Instance_SG_Tags = aws_security_group.sg.tags_all
         os_user = local.os_user
         ansible_connection = local.ansible_connection
         # key_status = data.external.check_key_pair.result.exists == "false" ? "New Created" : "Already Exist"
         #test= data.external.check_key_pair.result.exists
     }
+}
+
+output "instance_tags" {
+    description = "All tags applied to the EC2 instance (tags_all)"
+    value       = aws_instance.server.tags_all
+}
+
+output "resource_tags" {
+    description = "Common resource tag map from locals.resource_tags"
+    value       = local.resource_tags
+}
+
+output "ec2_tags" {
+    description = "EC2-specific tag map (locals.ec2_tags)"
+    value       = local.ec2_tags
 }
